fe3ed9397e6e984cfea283f0699e33a0f1f315cb
Nick Shryock <nickshryock@Nicks-Air.lan>
2022-12-30 20:29:03 -0500

    changing production pool

--- db/index.js
+++ db/index.js
@@ -11,15 +11,12 @@ const devPool = new Pool({
     idleTimeoutMillis : 30000
 });
 
-const prodPool = new Pool({
-    user:'ekhgefmsijkfnz',
-    host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
-    database: 'd17hdkp7fbogdp',
-    password: '280f9522a795fa37398235cc165aa3df3adc9aef6fe29f64de2ed7f8d126de08',
-    port: 5432,
-    connectionTimeoutMillis : 5000,
-    idleTimeoutMillis : 30000
-});
+const prodPool = new Pool({ // create connection to database
+    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+    ssl: {
+      rejectUnauthorized: false // don't check for SSL cert
+    }
+  });
 
 
 const listener = new Client({
