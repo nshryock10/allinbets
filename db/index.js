54ee03f21fa60acbc39a9e8c5b099e7128e13d7b
Nick Shryock <nickshryock@Nicks-Air.lan>
2022-12-30 22:06:14 -0500

    adding client listener

--- db/index.js
+++ db/index.js
@@ -30,13 +30,10 @@ const listener = new Client({
 });
 
 const prodListener = new Client({
-    user:'ekhgefmsijkfnz',
-    host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
-    database: 'd17hdkp7fbogdp',
-    password: '280f9522a795fa37398235cc165aa3df3adc9aef6fe29f64de2ed7f8d126de08',
-    port: 5432,
-    connectionTimeoutMillis : 5000,
-    idleTimeoutMillis : 30000
+    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+    ssl: {
+      rejectUnauthorized: false // don't check for SSL cert
+    }
 });
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,13 @@ router.get("/", (req, res, next) => {
     res.json({ info: 'Node.js, Express, and Postgres API for HotGuyPropBets' });
 })
 
-/*
 //Listens for updates to final answers and updates scores
-db.listener.connect(() => console.log('Database connected!'));
-db.listener.query('LISTEN answer_update');
-db.listener.on('notification', () => {
+db.prodListener.connect(() => console.log('Database connected!'));
+db.prodListener.query('LISTEN answer_update');
+db.prodListener.on('notification', () => {
+    console.log('Database question updated...')
     queries.updateScores()
 });
-*/
 
 //Get all users
 router.get('/users', queries.getAllUsers);
